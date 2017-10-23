acdac497c4628d8e14ad11ab8d3ddee04191cc56
Steffest <dev@stef.be>
2017-10-23 16:19:51 +0200

    detect AGA example

---
+++ detectAGA/detect.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <proto/exec.h>
+#include <graphics/gfxbase.h>
+
+struct GfxBase *GraphicsBase;
+
+main()
+{
+    int isAGA = 0;
+
+    // first AGA Amiga had V39
+    // V37 is found in kickstart 2.04
+    // V34 in kickstart 1.3
+    int version = 39;
+    GraphicsBase=(struct GfxBase *)OpenLibrary("graphics.library",version);
+
+    if (GraphicsBase){
+        printf("Graphics.library V%d OK \n",version);
+        isAGA = (GraphicsBase->ChipRevBits0 & GFXF_AA_ALICE) > 0;
+        printf("has AGA: %d \n",isAGA);
+        CloseLibrary((struct Library *)GraphicsBase);
+    }else{
+        printf("No AGA \n");
+        version = 37;
+        GraphicsBase=(struct GfxBase *)OpenLibrary("graphics.library",version);
+        if (GraphicsBase){
+            printf("Graphics.library V%d OK \n",version);
+            CloseLibrary((struct Library *)GraphicsBase);
+        }else{
+            printf("Graphics.library Version lower then 37 \n");
+        }
+    }
+
+    return 0;
+
+}
